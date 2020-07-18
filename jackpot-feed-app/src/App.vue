bd57a5f28496be4c13b542e4762bba2e36f385b6
Yuliyan Ivanov <winsomewe.kek37@gmail.com>
2020-07-18 14:49:35 +0300

    Initial project and two components created

---
+++ jackpot-feed-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <BigPot />
+    <MediumPot />
+    <MediumPot />
+  </div>
+</template>
+
+<script>
+import BigPot from "./components/BigPot/BigPot.vue";
+import MediumPot from "./components/MediumPot/MediumPot.vue";
+
+export default {
+  name: "App",
+  components: {
+    BigPot,
+    MediumPot
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ jackpot-feed-app/src/components/MediumPot/MediumPot.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container"></div>
+</template>
+
+<script>
+module.exports = {
+  data: function() {
+    return {
+      pot: "Medium Pot"
+    };
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
